087b9d3841bb1d189ad2c8f9763ec6886db0d4be
zhanghuize <56755115+zhanghuize-git@users.noreply.github.com>
2022-08-14 17:10:13 +0800

    chore: 增加所有组件样式

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { presetUno, presetAttributify, presetIcons } from "unocss";
+// import Unocss from "unocss/vite";
+import Unocss from "./config/unocss";
 
 // https://vitejs.dev/config/
-
 
 const rollupOptions = {
 
@@ -18,21 +20,27 @@ const rollupOptions = {
 export default defineConfig({
 
   plugins: [
-    vue(),
+    vue(),    // 添加JSX插件
     vueJsx({
       // options are passed on to @vue/babel-plugin-jsx
-    })
+    }),
+    // 添加UnoCSS插件
+    Unocss(),
   ],
   build: {
     rollupOptions,
-    minify:false,
+    minify: false,
     lib: {
       entry: "./src/entry.ts",
       name: "SmartyUI",
       fileName: "smarty-ui",
       // 导出模块格式
-      formats: ["esm", "umd","iife"],
+      formats: ["esm", "umd", "iife"],
     },
   },
-
+  resolve: {
+    alias: {
+      "vue": "vue/dist/vue.esm-bundler.js"
+    },
+  }
 });
