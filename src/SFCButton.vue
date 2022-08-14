fa9b8519ff7d37e6ae5ad5eb716ba43dc8029e90
zhanghuize <56755115+zhanghuize-git@users.noreply.github.com>
2022-08-14 14:31:17 +0800

    init project

---
+++ src/SFCButton.vue
@@ -0,0 +1,10 @@
+<template>
+  <button>SFC Button</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "SFCButton",
+});
+</script>
---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+
+
+const rollupOptions = {
+
+  external: ["vue", "vue-router"],
+  output: {
+    globals: {
+      vue: "Vue",
+    },
+  },
+};
+
+export default defineConfig({
+
+  plugins: [
+    vue(),
+    vueJsx({
+      // options are passed on to @vue/babel-plugin-jsx
+    })
+  ],
+  build: {
+    rollupOptions,
+    minify:false,
+    lib: {
+      entry: "./src/entry.ts",
+      name: "SmartyUI",
+      fileName: "smarty-ui",
+      // 导出模块格式
+      formats: ["esm", "umd","iife"],
+    },
+  },
+
+});
